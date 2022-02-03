0497cef2e11d906a0f3e4c22187d3bcaff79aba1
sagarikanaik <sagarika.naik@promantia.com>
2022-02-03 13:24:22 +0530

    11021/api file bug fix for erpnext module

--- hotset_customizations/__init__.py
+++ hotset_customizations/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 __version__ = '0.0.1'
-import erpnext.stock.doctype.serial_no.serial_no
+import erpnext
+from erpnext.stock.doctype.serial_no.serial_no import validate_serial_no as erpnext_validate_serial_no
 from hotset_customizations.api import validate_serial_no
 
-erpnext.stock.doctype.serial_no.serial_no.validate_serial_no=validate_serial_no
+erpnext_validate_serial_no=validate_serial_no
