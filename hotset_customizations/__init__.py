a0ca9751cf72deadba308a9d8c80dd097d6d0f08
sagarikanaik <sagarika.naik@promantia.com>
2022-03-02 18:14:02 +0530

    11021/Fix for init py file

--- hotset_customizations/__init__.py
+++ hotset_customizations/__init__.py
@@ -2,8 +2,7 @@
 from __future__ import unicode_literals
 __version__ = '0.0.1'
 import erpnext
-import erpnext.stock.doctype.serial_no.serial_no as erp
 from hotset_customizations.api import hotset_validate_serial_no
 
 
-erp.validate_serial_no=hotset_validate_serial_no
+erpnext.stock.doctype.serial_no.serial_no.validate_serial_no=hotset_validate_serial_no
